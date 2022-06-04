e70e26ae69fbdf45521c8a7ef2b0160f8953bf19
Kyle Coop <kylecoop152@gmail.com>
2022-06-04 19:49:32 +0100

    made the jam sticking better (and fixed the name (and updated the version number))

--- src/main/java/net/fabricmc/ctf/CookedTropicalFishMod.java
+++ src/main/java/net/fabricmc/ctf/CookedTropicalFishMod.java
@@ -5,7 +5,6 @@ import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
-import net.minecraft.block.HoneyBlock;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.Material;
 import net.minecraft.client.render.RenderLayer;
--- src/main/java/net/fabricmc/ctf/mixin/JamBlockMixin.java
+++ src/main/java/net/fabricmc/ctf/mixin/JamBlockMixin.java
@@ -6,12 +6,28 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.fabricmc.ctf.CookedTropicalFishMod;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 
 @Mixin(net.minecraft.block.piston.PistonHandler.class)
 public class JamBlockMixin {
-    @Inject(method = "isBlockSticky(Lnet/minecraft/block/BlockState;)Z", at = @At("RETURN"), cancellable = true)
+    @Inject(method = "isBlockSticky(Lnet/minecraft/block/BlockState;)Z", at = @At("HEAD"), cancellable = true)
     private static void injected(BlockState state, CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(cir.getReturnValue() || state.isOf(CookedTropicalFishMod.GLOW_BERRY_BLOCK));
+        if (state.isOf(CookedTropicalFishMod.GLOW_BERRY_BLOCK)) cir.setReturnValue(true);
+    }
+
+    @Inject(method = "isAdjacentBlockStuck(Lnet/minecraft/block/BlockState;Lnet/minecraft/block/BlockState;)Z", at = @At("HEAD"), cancellable = true)
+    private static void injected(BlockState state, BlockState adjacentState, CallbackInfoReturnable<Boolean> cir) {
+        if (state.isOf(CookedTropicalFishMod.GLOW_BERRY_BLOCK)) {
+            if (adjacentState.isOf(Blocks.HONEY_BLOCK) || adjacentState.isOf(Blocks.SLIME_BLOCK)) {
+                cir.setReturnValue(false);
+            }
+        }
+        if (adjacentState.isOf(CookedTropicalFishMod.GLOW_BERRY_BLOCK)) {
+            if (state.isOf(Blocks.HONEY_BLOCK) || state.isOf(Blocks.SLIME_BLOCK)) {
+                cir.setReturnValue(false);
+            }
+        }
     }
 }
